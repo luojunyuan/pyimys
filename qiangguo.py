26239ac8647ad7d6a1c00da2f1eaa56b49ca0f3e
k1mlka <ljy7723656@outlook.com>
2019-11-18 20:52:46 +0800

    Add files via upload

---
+++ qiangguo.py
@@ -0,0 +1,231 @@
+from com.android.monkeyrunner import MonkeyRunner
+import os
+device = MonkeyRunner.waitForConnection()
+
+# start
+# device.touch(350, 1250, 'DOWN_AND_UP')
+# MonkeyRunner.sleep(2)
+#news
+# device.touch(230, 140, 'DOWN_AND_UP')
+# MonkeyRunner.sleep(2)
+
+#return
+# device.press('KEYCODE_BACK','DOWN_AND_UP')
+
+#star
+# device.touch(600, 1250, 'DOWN_AND_UP')
+# MonkeyRunner.sleep(2)
+
+#share
+# device.touch(674, 1250, 'DOWN_AND_UP')
+# MonkeyRunner.sleep(2)
+# device.touch(90, 857, 'DOWN_AND_UP')
+# MonkeyRunner.sleep(2)
+# device.touch(134, 453, 'DOWN_AND_UP')
+# MonkeyRunner.sleep(2)
+# device.touch(600, 788, 'DOWN_AND_UP')
+# MonkeyRunner.sleep(2)
+
+#################################################
+# follow
+device.touch(215, 1250, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#begin 1
+print('1')
+device.touch(222, 205, 'DOWN_AND_UP')
+MonkeyRunner.sleep(5)
+#share
+#star
+#wait
+MonkeyRunner.sleep(130)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#2
+print('2')
+device.touch(282, 373, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#share
+#star
+#wait
+MonkeyRunner.sleep(130)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#3
+print('3')
+device.touch(338, 593, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(130)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+print('four')
+#4
+device.touch(321, 884, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(130)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#5
+print('five')
+device.touch(359, 1143, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(130)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+
+#6
+print('drag')
+device.drag((300,1100),(200,100),2)
+MonkeyRunner.sleep(2)
+device.touch(190, 476, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(130)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#7
+device.touch(179, 649, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(130)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#8
+evice.touch(253, 887, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(130)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#9
+evice.touch(281, 1144, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(130)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+##############################################
+MonkeyRunner.sleep(2)
+print('video')
+video
+device.touch(500, 1225, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+device.touch(313, 139, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#1
+device.touch(358, 412, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+
+#2
+device.touch(342, 750, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+
+#3
+device.touch(151, 910, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+
+#4
+device.touch(165, 1036, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+
+#5
+device.touch(229, 1153, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+
+# 6
+device.drag((300,1100),(200,100),2)
+MonkeyRunner.sleep(2)
+device.touch(123, 376, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#7
+device.touch(126, 504, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+# 8
+device.touch(129, 632, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#9
+device.touch(213, 726, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#10
+device.touch(194, 875, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#11
+device.touch(198, 1019, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#12
+device.touch(217, 1156, 'DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+#wait
+MonkeyRunner.sleep(200)
+#return
+device.press('KEYCODE_BACK','DOWN_AND_UP')
+MonkeyRunner.sleep(2)
+
+os.system("shutdown -s -t  60 ")
+# shutdown -a
